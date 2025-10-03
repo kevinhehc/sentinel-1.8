198dfc65efc5033256a138f1778fc4aec01380be
kevinhehc <143791417+kevinhehc@users.noreply.github.com>
2025-10-04 02:10:39 +0800

    EntryType的作用，出口流量和入口流量做的处理不一样

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/EntryType.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/EntryType.java
@@ -24,10 +24,16 @@ package com.alibaba.csp.sentinel;
 public enum EntryType {
     /**
      * Inbound traffic
+     * 表示“入口流量”或“入站调用”。
+     * 一般是指外部请求进入本服务（比如 API 接口、RPC 服务端接收的请求）。
+     * Sentinel 在处理入口流量时会进行系统保护（SystemRule）、流量控制等，防止整体系统过载。
      */
     IN,
     /**
      * Outbound traffic
+     * 表示“出口流量”或“出站调用”。
+     * 指本服务调用外部依赖（如访问数据库、调用下游服务的 RPC/HTTP）。
+     * Sentinel 在处理出口流量时，更多是做依赖保护（例如给下游调用加限流、熔断降级），避免因为下游慢或不可用导致本地雪崩。
      */
     OUT;
 
