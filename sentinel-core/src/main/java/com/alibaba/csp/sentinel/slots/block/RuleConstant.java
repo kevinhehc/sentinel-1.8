58cf9ddc5165b9e57057bb75fac1f0bd4fab50f5
kevinhehc <143791417+kevinhehc@users.noreply.github.com>
2025-10-05 00:02:42 +0800

    流控模式=链路的解说

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/RuleConstant.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/RuleConstant.java
@@ -42,8 +42,18 @@ public final class RuleConstant {
     public static final int AUTHORITY_WHITE = 0;
     public static final int AUTHORITY_BLACK = 1;
 
+    // 直接针对资源本身。 对某个资源（比如接口 A）单独做限流，不管是谁调用。
     public static final int STRATEGY_DIRECT = 0;
+    // 根据关联资源。 对一个资源 A 的流量控制，会参考另一个资源 B 的情况。比如，当 B 超过阈值时，对 A 也限流。
     public static final int STRATEGY_RELATE = 1;
+    // 根据调用链入口
+    // 只在指定的调用链入口下，对目标资源做流控。
+    // 换句话说，只有当资源是从某个入口资源调用进来的时候，才会触发限流。
+    // 举个例子：
+    //	•	系统里有两个入口：入口1 -> ServiceA，入口2 -> ServiceA。
+    //	•	如果你给 ServiceA 设置链路模式，并指定入口是 入口1：
+    //	        • 当 入口1 调用 ServiceA 时，限流规则会生效。
+    //	        • 当 入口2 调用 ServiceA 时，不受这个规则约束。
     public static final int STRATEGY_CHAIN = 2;
 
     public static final int CONTROL_BEHAVIOR_DEFAULT = 0;
